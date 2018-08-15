aa483254c187e2a61623c476d99cba5f1798bfbd
Christopher Stull <chris.stull@gmail.com>
2018-08-15 18:12:38 +0000

    automatically backed up by learn

--- collections_practice.rb
+++ collections_practice.rb
@@ -3,7 +3,8 @@ def begins_with_r(collection)
   collection.each do |element|
     if element.upcase.start_with?('R')
     else
-    return false
+      return false
+      break
     end
     return true
   end
