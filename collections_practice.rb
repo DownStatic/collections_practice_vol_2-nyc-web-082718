28a9943a0ad3ca07242ce9ec749409f815d34645
Christopher Stull <chris.stull@gmail.com>
2018-08-15 18:08:00 +0000

    automatically backed up by learn

--- collections_practice.rb
+++ collections_practice.rb
@@ -1,1 +1,10 @@
 # your code goes here
+def begins_with_r(collection)
+  collection.each do |element|
+    if element.first == 'r' or element.first == 'R'
+      pass
+    else
+      return false
+    end
+    return true
+  end
