23df189747a2d5fe1f023f79ef0a79dcad048cb6
Christopher Stull <chris.stull@gmail.com>
2018-08-15 18:59:45 +0000

    Done.

--- collections_practice.rb
+++ collections_practice.rb
@@ -1,1 +1,9 @@
 # your code goes here
+def begins_with_r(collection)
+  collection.any? do |element|
+    if not element.upcase.start_with?('R')
+      return false
+    else
+      return true
+  end
+end
